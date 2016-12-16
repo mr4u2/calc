7ed1ff7aac3de6902121d322d862406860bedfbf
mr4u2 <mr4u2@users.noreply.github.com>
2016-12-16 23:45:26 +0100

    In calc2 clone added - and changed later in Calc - and updated in calc2

--- calc.py
+++ calc.py
@@ -3,6 +3,7 @@ def add(x, y):
 
 # This should have been added as feature2 but added under divide now
 
+# This is added in Calc2 clone ----and now this again in Calc----- and modified in Calc later!!!
 
 def subtract(x, y):
     return x - y
